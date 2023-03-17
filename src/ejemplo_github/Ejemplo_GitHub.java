146892933e223ce6ebd86ebce4bd8e3b3b5fdaeb
José Sánchez <Jos Snchez@DESKTOP-LVI50Q6>
2023-03-16 18:19:29 -0600

    https://github.com/Jose14DSS/Ejemplo_GitHub.git

---
+++ src/ejemplo_github/Ejemplo_GitHub.java
@@ -0,0 +1,17 @@
+
+package ejemplo_github;
+
+/**
+ *
+ * @author José Sánchez
+ */
+public class Ejemplo_GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
